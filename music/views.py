8c1ee32450d8e8f9cb8c50c0385f77b61fa80430
jaykishan1 <jayakishan100@gmail.com>
2016-10-11 01:44:07 +0530

    Http 404 Short cut

--- music/views.py
+++ music/views.py
@@ -1,6 +1,6 @@
 from django.http import Http404
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Album
 
 
@@ -13,10 +13,11 @@ def index(request):
 
 
 def detail(request, album_id):
-    try:
-        album = Album.objects.get(pk=album_id)
+    #try:
+    #    album = Album.objects.get(pk=album_id)
 
-    except Album.DoesNotExist:
-        raise Http404("Album Doesnt Exist")
+    #except Album.DoesNotExist:
+    #    raise Http404("Album Doesnt Exist")
+    album = get_object_or_404(Album, pk=album_id)
 
     return render(request, 'music/detail.html', {'album': album})
