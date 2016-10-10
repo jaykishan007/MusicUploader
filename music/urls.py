495f214dd8fca81efbf788511d4932dc27f14737
jaykishan1 <jayakishan100@gmail.com>
2016-10-11 03:11:03 +0530

    Choice of Fav Song

--- music/urls.py
+++ music/urls.py
@@ -6,7 +6,11 @@ app_name='music'
 urlpatterns = [
     #/music/
     url(r'^$', views.index, name='index'),
-    #/music/55/
+
+    #/music/<album_id>/
     url(r'^(?P<album_id>[0-9]+)/$', views.detail, name='detail'),
+
+    #/music/<album_id>/favorite/
+    url(r'^(?P<album_id>[0-9]+)/favorite/$', views.favorite, name='favorite'),
 
 ]
